f58ff69710961f983ccc216fadcc74257b23900f
kahhhh666 <kauehenriquelima23@gmail.com>
2026-08-28 16:38:24 -0300

    Create Gerente.cs

---
+++ slnTask/AppTaskZero/Models/Gerente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppTaskZero.Models;
+
+public partial class Gerente
+{
+    public int Codigo { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public string Setor { get; set; } = null!;
+
+    public virtual ICollection<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
+}
