63683e0d9131868b91d67f10750ac01de6eec163
kahhhh666 <kauehenriquelima23@gmail.com>
2026-08-28 16:38:22 -0300

    Update Funcionario.cs

--- slnTask/AppTaskZero/Models/Funcionario.cs
+++ slnTask/AppTaskZero/Models/Funcionario.cs
@@ -11,5 +11,11 @@ public partial class Funcionario
 
     public string Cargo { get; set; } = null!;
 
+    public int? GerenteId { get; set; }
+
+    public virtual Gerente? Gerente { get; set; }
+
+    public virtual ICollection<Incidente> Incidentes { get; set; } = new List<Incidente>();
+
     public virtual ICollection<Tarefa> Tarefas { get; set; } = new List<Tarefa>();
 }
