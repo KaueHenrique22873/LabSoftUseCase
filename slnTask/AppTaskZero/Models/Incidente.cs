5251c16dd8067205bf8ee4f133ea106475def4c0
kahhhh666 <kauehenriquelima23@gmail.com>
2026-08-28 16:38:27 -0300

    Create Incidente.cs

---
+++ slnTask/AppTaskZero/Models/Incidente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppTaskZero.Models;
+
+public partial class Incidente
+{
+    public int Codigo { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string Descricao { get; set; } = null!;
+
+    public string Prioridade { get; set; } = null!;
+
+    public string StatusIncidente { get; set; } = null!;
+
+    public DateTime DataAbertura { get; set; }
+
+    public DateTime? DataResolucao { get; set; }
+
+    public int FuncionarioId { get; set; }
+
+    public int? TarefaId { get; set; }
+
+    public virtual Funcionario Funcionario { get; set; } = null!;
+
+    public virtual Tarefa? Tarefa { get; set; }
+}
