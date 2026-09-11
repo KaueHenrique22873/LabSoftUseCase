4200bd44e883ad2efda9dd292278181b2229b090
kahhhh666 <kauehenriquelima23@gmail.com>
2026-09-11 14:40:54 -0300

    Atualiza consulta para n dar erro

--- SLNTask/AppClinica/Models/Consulta.cs
+++ SLNTask/AppClinica/Models/Consulta.cs
@@ -15,7 +15,7 @@ public partial class Consulta
 
     public int MedicoId { get; set; }
 
-    public virtual Medico Medico { get; set; } = null!;
+    public virtual Medico? Medico { get; set; }
 
-    public virtual Paciente Paciente { get; set; } = null!;
+    public virtual Paciente? Paciente { get; set; } 
 }
