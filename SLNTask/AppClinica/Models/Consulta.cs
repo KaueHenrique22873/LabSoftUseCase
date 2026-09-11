67ddc953f469061a083fe54a8efb70d5893f1864
kahhhh666 <kauehenriquelima23@gmail.com>
2026-09-11 14:13:56 -0300

    Corrigindo erro de sintaxe do Consulta

---
+++ SLNTask/AppClinica/Models/Consulta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppClinica.Models;
+
+public partial class Consulta
+{
+    public int Codigo { get; set; }
+
+    public DateTime DataHora { get; set; }
+
+    public string StatusConsulta { get; set; } = null!;
+
+    public int PacienteId { get; set; }
+
+    public int MedicoId { get; set; }
+
+    public virtual Medico Medico { get; set; } = null!;
+
+    public virtual Paciente Paciente { get; set; } = null!;
+}
---
+++ SLNTask/AppClinica/Models/DbclinicaContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppClinica.Models;
+
+public partial class DbclinicaContext : DbContext
+{
+    public DbclinicaContext()
+    {
+    }
+
+    public DbclinicaContext(DbContextOptions<DbclinicaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Consulta> Consulta { get; set; }
+
+    public virtual DbSet<Medico> Medicos { get; set; }
+
+    public virtual DbSet<Paciente> Pacientes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConexaoSqlServer");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Consulta>(entity =>
+        {
+            entity.HasKey(e => e.Codigo).HasName("PK__Consulta__06370DAD117C2DDB");
+
+            entity.Property(e => e.DataHora).HasColumnType("datetime");
+            entity.Property(e => e.StatusConsulta)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Medico).WithMany(p => p.Consulta)
+                .HasForeignKey(d => d.MedicoId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Consulta_Medico");
+
+            entity.HasOne(d => d.Paciente).WithMany(p => p.Consulta)
+                .HasForeignKey(d => d.PacienteId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Consulta_Paciente");
+        });
+
+        modelBuilder.Entity<Medico>(entity =>
+        {
+            entity.HasKey(e => e.Codigo).HasName("PK__Medico__06370DAD3D01765F");
+
+            entity.ToTable("Medico");
+
+            entity.Property(e => e.Crm)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("CRM");
+            entity.Property(e => e.Especialidade)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Nome)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Paciente>(entity =>
+        {
+            entity.HasKey(e => e.Codigo).HasName("PK__Paciente__06370DAD2A2A37AF");
+
+            entity.ToTable("Paciente");
+
+            entity.Property(e => e.Cpf)
+                .HasMaxLength(200)
+                .IsUnicode(false)
+                .HasColumnName("CPF");
+            entity.Property(e => e.Nome)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefone)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
