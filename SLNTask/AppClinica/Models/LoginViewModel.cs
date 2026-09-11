7e0da6ad6bc7ea330502b648fb79131da4e01d1b
kahhhh666 <kauehenriquelima23@gmail.com>
2026-09-11 16:25:11 -0300

    Update LoginViewModel.cs

--- SLNTask/AppClinica/Models/LoginViewModel.cs
+++ SLNTask/AppClinica/Models/LoginViewModel.cs
@@ -1,7 +1,4 @@
-﻿using AppClinica.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
 namespace AppClinica.Models
 {
     public class LoginViewModel
@@ -9,55 +6,5 @@ namespace AppClinica.Models
         [Required(ErrorMessage = "O CPF é obrigatório.")]
         [Display(Name = "CPF do Paciente")]
         public string Cpf { get; set; } = string.Empty;
-    }
-}
-
-
-
-
-
-
-namespace appReverso.Controllers
-{
-    public class AccountController : Controller
-    {
-        private readonly DbclinicaContext _context;
-
-        public AccountController(DbclinicaContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public IActionResult Login() => View();
-
-
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            if (!ModelState.IsValid) return View(model);
-
-            var paciente = await _context.Pacientes
-                .FirstOrDefaultAsync(p => p.Cpf == model.Cpf);
-
-            if (paciente == null)
-            {
-                ModelState.AddModelError("", "CPF não encontrado.");
-                return View(model);
-            }
-
-            // Salva na Session
-            HttpContext.Session.SetInt32("PacienteId", paciente.Codigo);
-            HttpContext.Session.SetString("PacienteNome", paciente.Nome);
-
-            return RedirectToAction("Index", "Consulta");
-        }
-
-        public IActionResult Logout()
-        {
-            HttpContext.Session.Clear(); // Limpa a sessão
-            return RedirectToAction("Login");
-        }
-
     }
 }
